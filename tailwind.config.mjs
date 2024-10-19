2d4e75148280b155a3b2ffe403d9204e8ece5844
CodeArmer <geanmotas@gmail.com>
2024-10-19 01:21:40 -0400

    Installed font and added to the tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			colors: {
 				pumking: "#cd4c3a"
+			},
+			fontFamily: {
+				sans: ['Space Grotesk Variable', ...defaultTheme.fontFamily.sans]
 			}
 		},
 	},
